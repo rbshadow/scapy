513c3a25a1af8c627c2fd173722b7da5d0aa3eca
root <root@localhost.localdomain>
2017-08-26 17:32:13 -0400

    Added fragment functionality

--- frag.py
+++ frag.py
@@ -6,14 +6,20 @@
 #======================================#
 
 import sys
-from scapy.all import sr1,IP,ICMP,TCP,Raw,sendp
+from scapy.all import sr1,IP,ICMP,TCP,Raw,send,fragment
 
 if len(sys.argv) !=2:
     print "ERROR: please enter arguments: "
     print "Usage: ./frag.py <ipaddr>"
     sys.exit(1)
 
+# Create a TCP packet and load your data 
 data = "This is a test string"
-p = IP(dst=sys.argv[1]) / TCP() / Raw(load=data)
-sendp(p)
-p.show()
+packet = IP(dst=sys.argv[1]) / TCP() / Raw(load=data)
+
+#Fragment the packet
+frags = fragment(packet,fragsize=16)
+for f in frags:
+    f.show()
+    send(f)
+    f.show()
